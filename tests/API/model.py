633ee56c43b3f19f767a1378467cc0862f5a5d9f
rexionmars <opensource.leonardi@gmail.com>
2023-08-13 19:32:33 -0300

    feat: update change for api model

--- tests/API/model.py
+++ tests/API/model.py
@@ -1,51 +1,41 @@
-import json
-from flask import Flask, jsonify
+class Model:
+    """
+    Return an fake Vital Signs Monitor informations
+    """
+    def vital_signs_monitor():
+        ECG = 80  # Heart rate
+        RESP = 14  # Resparation rate
+        SPO2 = 99  # Oxygen saturation (%)
+        CO2 = 38  # Carbon dioxide
 
-app = Flask(__name__)
+        # Ivansive Blood Presure
+        ART_IBP = "120/80"
+        ART_MMHG = 93
+        CVP_IBP = "120/80"
+        CVP_MMHG = 93
 
+        NIMP = "120/80"  # Non-invasive Blood Presure
+        NIMP_MMHG = 93
 
-ECG = 80  # Heart rate
-RESP = 14  # Resparation rate
-SPO2 = 99  # Oxygen saturation (%)
-CO2 = 38  # Carbon dioxide
+        signals: dict = {
+            "ECG bpm": ECG,
+            "RESP": RESP,
+            "SpO2": SPO2,
+            "Co2": CO2,
+            "IBP": {
+                "ART": {
+                    "mmHg": ART_MMHG,
+                    "sys": ART_IBP,
+                },
+                "CVP": {
+                    "mmHg": CVP_MMHG,
+                    "sys": CVP_IBP,
+                },
+            },
+            "NIMP": {
+                "mmHg": NIMP_MMHG,
+                "sys": NIMP,
+            }
+        }
 
-# Ivansive Blood Presure
-ART_IBP = "120/80"
-ART_MMHG = 93
-CVP_IBP = "120/80"
-CVP_MMHG = 93
-
-NIMP = "120/80"  # Non-invasive Blood Presure
-NIMP_MMHG = 93
-
-
-signals: dict = {
-    "ECG bpm": ECG,
-    "RESP": RESP,
-    "SpO2": SPO2,
-    "Co2": CO2,
-    "IBP": {
-        "ART": {
-            "mmHg": ART_MMHG,
-            "sys": ART_IBP,
-        },
-        "CVP": {
-            "mmHg": CVP_MMHG,
-            "sys": CVP_IBP,
-        },
-    },
-    "NIMP": {
-        "mmHg": NIMP_MMHG,
-        "sys": NIMP,
-    }
-}
-
-
-@app.route('/monitor')
-def api():
-    _ = json.dumps(signals, indent=4)
-    return jsonify(signals), 200
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        return signals
