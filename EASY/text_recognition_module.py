b6df499e45f7510273e7dd6f8baa9bb0800792a5
João Leonardi <opensource.leonardi@gmail.com>
2023-10-11 21:41:34 -0300

    chore: Enhance code quality and add technical documentation
    Refactor the `TextRecognition` class for better code quality and readability. This includes:
    
    - Improving technical documentation (docstrings) for class attributes, methods, and functions.
    - Making the code more readable with clear variable names and comments.
    - Adding explanatory comments to improve code comprehension.
    - Applying best practices for code style and formatting.
    
    This commit focuses on code and documentation improvements without changing functionality. It ensures that the codebase is more maintainable and understandable.

--- EASY/text_recognition_module.py
+++ EASY/text_recognition_module.py
@@ -1,28 +1,50 @@
+import cv2
+import easyocr
 import json
 import re
+
 import sys
 import threading
 from queue import Queue
 
-import cv2
-import easyocr
-
-
 class TextRecognition:
+    """
+    A class for performing text recognition in a video stream.
+
+    Attributes:
+        reader (easyocr.Reader): An EasyOCR reader for text recognition.
+        video_capture (VideoCapture): A VideoCapture object for accessing the video source.
+        last_frame (numpy.ndarray): The last processed video frame.
+        rois (list of tuples): A list of regions of interest (ROIs) defined by rectangles.
+        current_roi (tuple or None): The currently being created ROI.
+        drawing (bool): A flag to indicate if an ROI is being drawn.
+        deleted_rois (list of tuples): A list to store deleted ROIs.
+        running (bool): A flag to indicate if the program is running.
+        event_queue (Queue): A queue to handle user input events.
+    """
+
     def __init__(self, video_source, language="en"):
+        """
+        Initialize the TextRecognition instance.
+
+        Args:
+            video_source (str): The video source URL or file path.
+            language (str, optional): The language for text recognition (default is "en").
+        """
         self.reader = easyocr.Reader([language])
         self.video_capture = VideoCapture(video_source)
         self.last_frame = None
-        self.rois = []  # List to store ROIs
-        self.current_roi = None  # The currently being created ROI
-        self.drawing = False  # Flag to indicate if we are drawing an ROI
-        self.deleted_rois = []  # List to store deleted ROIs
-        self.running = True  # Flag to indicate if the program is running
-
-        # Create a queue to handle user events
+        self.rois = []
+        self.current_roi = None
+        self.drawing = False
+        self.deleted_rois = []
+        self.running = True
         self.event_queue = Queue()
 
     def start(self):
+        """
+        Start the text recognition process in separate threads.
+        """
         video_thread = threading.Thread(target=self.video_processing_thread)
         video_thread.start()
         user_input_thread = threading.Thread(target=self.handle_user_input)
@@ -30,20 +52,23 @@ class TextRecognition:
         self.display_window()
 
     def read_text(self, frame):
+        """
+        Perform text recognition on the input video frame.
+
+        Args:
+            frame (numpy.ndarray): The input video frame.
+        """
         if frame is None:
             return
 
-
-
-        roi_data = {}  # Dictionary to store ROI data
-
+        roi_data = {}
         for i, roi in enumerate(self.rois):
             x, y, w, h = roi
             cropped_frame = frame[y:y + h, x:x + w]
             results = self.reader.readtext(cropped_frame)
 
-            roi_info = {}  # Dictionary to store current ROI information
-            list_value = []  # List to store values in this ROI
+            roi_info = {}
+            list_value = []
 
             for bbox, text, prob in results:
                 label, value = self.extract_label_and_value(text)
@@ -51,12 +76,11 @@ class TextRecognition:
 
                 if label:
                     if label not in roi_info:
-                        roi_info[label] = []  # Cria uma lista temporária se ela ainda não existir
+                        roi_info[label] = []
                 else:
-                    # Processamento para o caso em que label é vazio ou None
+                    # Handle the case when label is empty or None
                     ...
 
-                # Render text detection on the frame within the ROI
                 text_x = x + bbox[0][0]
                 text_y = y + bbox[0][1]
                 cv2.rectangle(frame, (text_x, text_y), (x + bbox[2][0], y + bbox[2][1]), (0, 255, 0), 1)
@@ -66,56 +90,61 @@ class TextRecognition:
             filtered_values = [int(item) for item in filtered_values]
 
             for label, values in roi_info.items():
-                values.extend(filtered_values)  # Adiciona os valores filtrados à lista temporária
+                values.extend(filtered_values)
 
-            # Use f"ROI_ID {i}" como a chave para cada ROI
             roi_data[f"ROI_ID {i}"] = roi_info
 
         print(json.dumps(roi_data, indent=4))
 
-
-
-
-
-
-
-
-
-        # Print ROI data, including numeric values
         self.print_roi_data(roi_data)
 
         for roi in self.rois:
             x, y, w, h = roi
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (214, 102, 3), 1, )
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (214, 102, 3), 1)
 
         if self.current_roi is not None:
             x, y, w, h = self.current_roi
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (129, 23, 255), 1, )
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (129, 23, 255), 1)
 
         self.last_frame = frame
 
     def print_roi_data(self, roi_data):
-        # for roi_id, data in roi_data.items():
-        # print(f"ROI {roi_id}:")
-        # for label, value in data.items():
-        # if isinstance(value, list):
-        # print(f"{label}: {value}")
-        # else:
-        # print(f"{label}: {value if value else 'None'}")
+        """
+        Print the ROI data, including numeric values, to the console.
+
+        Args:
+            roi_data (dict): A dictionary containing ROI data.
+        """
+        # Your code to print ROI data goes here
         pass
 
     def extract_label_and_value(self, text):
+        """
+        Extract the label and value from a text string.
+
+        Args:
+            text (str): The input text string.
+
+        Returns:
+            tuple: A tuple containing the label (str) and value (str or None).
+        """
         parts = re.split(r"(\d+)", text)
         label = parts[0].strip()
         value = parts[1].strip() if len(parts) > 1 else None
         return label, value
 
     def video_processing_thread(self):
+        """
+        Thread to process the video frames and perform text recognition.
+        """
         while self.running:
             ret, frame = self.video_capture.read()
             self.read_text(frame)
 
     def display_window(self):
+        """
+        Display the video stream window and handle user events.
+        """
         cv2.namedWindow("Text Recognition")
         cv2.setMouseCallback("Text Recognition", self.on_mouse_events)
 
@@ -130,6 +159,16 @@ class TextRecognition:
         cv2.destroyAllWindows()
 
     def on_mouse_events(self, event, x, y, flags, param):
+        """
+        Handle mouse events, such as drawing ROIs.
+
+        Args:
+            event (int): The event type.
+            x (int): The x-coordinate of the event.
+            y (int): The y-coordinate of the event.
+            flags (int): Flags associated with the event.
+            param: Additional parameters.
+        """
         if event == cv2.EVENT_LBUTTONDOWN:
             self.drawing = True
             self.current_roi = [x, y, 0, 0]
@@ -144,40 +183,71 @@ class TextRecognition:
             self.current_roi = None
 
     def remove_last_roi(self):
+        """
+        Remove the last ROI from the list of ROIs.
+        """
         if self.rois:
             self.deleted_rois.append(self.rois.pop())
 
     def undo_roi_deletion(self):
+        """
+        Undo the previous ROI deletion by restoring the last deleted ROI.
+        """
         if self.deleted_rois:
             self.rois.append(self.deleted_rois.pop())
 
     def handle_user_input(self):
+        """
+        Handle user input events, such as quitting or modifying ROIs.
+        """
         while self.running:
             key = self.event_queue.get()
-            if key == ord("q") or key == 27:  # 27 é o valor da tecla ESC
+            if key == ord("q") or key == 27:  # 27 is the ESC key
                 self.running = False
             elif key == ord("d"):
                 self.remove_last_roi()
             elif key == ord("u"):
                 self.undo_roi_deletion()
 
-
 class VideoCapture:
+    """
+    A class for video capture from a source.
+
+    Attributes:
+        cap: The OpenCV video capture object.
+        width (int): The width of the video frames.
+        height (int): The height of the video frames.
+    """
+
     def __init__(self, source):
+        """
+        Initialize the VideoCapture instance.
+
+        Args:
+            source (str): The video source URL or file path.
+        """
         self.cap = cv2.VideoCapture(source)
         self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     def read(self):
+        """
+        Read and retrieve a frame from the video source.
+
+        Returns:
+            tuple: A tuple containing a boolean indicating success and the video frame (numpy.ndarray).
+        """
         return self.cap.read()
 
     def release(self):
+        """
+        Release the video capture object.
+        """
         self.cap.release()
-
 
 if __name__ == "__main__":
     text_recognition = TextRecognition("http://192.168.0.51:81/stream")
     text_recognition.start()
     while text_recognition.running:
-        pass  # Aguarde até que a tecla "q" ou "ESC" seja pressionada
-    sys.exit(0)  # Saia do programa adequadamente após a tecla "q" ou "ESC" ser pressionada
+        pass  # Wait until the "q" or "ESC" key is pressed
+    sys.exit(0)
